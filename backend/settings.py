7662f35cb049ae9120a78dbbd014bfee72e5d094
gustavoeficiente <gustavo@eficienteserv.com>
2026-04-05 13:42:32 -0300

    Corrige admin static no Render

--- backend/settings.py
+++ backend/settings.py
@@ -85,7 +85,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
