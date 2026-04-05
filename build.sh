2a814e97600a26d088337cb9741c2312b526aad9
gustavoeficiente <gustavo@eficienteserv.com>
2026-04-05 14:38:10 -0300

    Corrige criação automática do superuser

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
-python backend/create_admin.py
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); username='gustavo'; email='seuemail@email.com'; password='12345678'; User.objects.filter(username=username).exists() or User.objects.create_superuser(username, email, password)"
